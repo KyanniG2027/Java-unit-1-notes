36a1a64276fb9ddde7288fa72d0e0e839b5bd96c
Kyanni <kyanni.gonzalez2027@gbwl.org>
2025-09-12 12:56:11 +0000

    1.2 Notes

--- Main.java
+++ Main.java
@@ -4,6 +4,22 @@ public class Main {
    public static void main(String []args) {
       System.out.println("Welcome to Java!");
       System.out.println("hiiii");
+ 
+      // 1. DECLARE a new variable
+      int score; 
+      // 2. ASSIGN a value to the variable
+      score = 13;
+      // 3. INITIALIZE combines declaring + assigning in one step
+      String pokemon = "Pikachu";
+      double healthPercent = 0.75; 
+      boolean isAlive = true;
+
+
+   // use the plus sign to CONCATENATE (combine) Strings
+   String greeting = "Hello " + "World";
+   System.out.println(greeting);
+   //Quotation marks contain STRING LITERALS
+   
    }
 }
 
